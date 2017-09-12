d20ef95a3a517d2eb3012dba4977d40f61c75758
Jeff Lueck <lueckcfd@yahoo.com>
2017-09-12 10:15:18 -0500

    initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,32 @@
+const React = require('react');
+const {Link, IndexLink} = require('react-router');
+
+const Nav = () => {
+  return(
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer app
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="" target="_blank"> Jeff Lueck</a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  )
+};
+
+module.exports = Nav;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'script-loader!jquery/dist/jquery.min.js',
+    'script-loader!foundation-sites/dist/js/foundation.min.js',
+    './app/app.jsx',
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin ({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    mainFiles: ["index"],
+    alias: {
+      Main: path.resolve(__dirname, 'app/components/Main.jsx'),
+      Nav: path.resolve(__dirname,'app/components/Nav.jsx'),
+      applicationStyles: path.resolve(__dirname, 'app/styles/app.scss')
+    },
+    extensions: ['*', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      },
+    ]
+  },
+  devtool: '#cheap-module-source-map'
+};
